230b83ada617a54413798f9e8f0b20f7d3517797
Mike <przero3@hotmail.com>
2023-11-01 21:11:27 -0300

    Default user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        // create default user
+        User::create(['name' => 'Administrator', 'email' => 'admin@admin.com', 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi']);
+
+        // create many articles
         \App\Models\Article::factory(150)->create();
 
     }
